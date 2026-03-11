ed8fec18009dae3f1baa25e62ce6e3db28fbf420
DangQuangSE <quangndse180246@fpt.edu.vn>
2026-03-11 10:53:17 +0700

    add

---
+++ SP26_PRN222_SE1816_ASM04_SE180246_QuangND/FManagement.BlazorWebApp.QuangND/Program.cs
@@ -0,0 +1,36 @@
+﻿using FManagement.BlazorWebApp.QuangND.Components;
+using FManagement.Services.QuangND;
+using Microsoft.EntityFrameworkCore;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+        builder.Services.AddScoped<IProductPlanQuangNDService, ProductPlanQuangNDService>();
+        builder.Services.AddScoped<StoreOrderItemQuangNDService>();
+        var app = builder.Build();
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error", createScopeForErrors: true);
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+            app.UseMigrationsEndPoint();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
+}
