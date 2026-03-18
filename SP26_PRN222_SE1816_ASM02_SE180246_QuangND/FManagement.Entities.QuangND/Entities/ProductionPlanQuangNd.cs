e400d737433441c4cebc46d2e40115a8df029c09
DangQuangSE <quangndse180246@fpt.edu.vn>
2026-03-18 10:35:31 +0700

    fix

--- SP26_PRN222_SE1816_ASM02_SE180246_QuangND/FManagement.Entities.QuangND/Entities/ProductionPlanQuangNd.cs
+++ SP26_PRN222_SE1816_ASM02_SE180246_QuangND/FManagement.Entities.QuangND/Entities/ProductionPlanQuangNd.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace FManagement.Entities.QuangND.Entities;
 
@@ -7,8 +8,10 @@ public partial class ProductionPlanQuangNd
 {
     public int PlanId { get; set; }
 
+    [Required(ErrorMessage = "StoreOrderItemId is required!")]
     public int StoreOrderItemId { get; set; }
 
+    [Required(ErrorMessage = "KitchenId is required!")]
     public int KitchenId { get; set; }
 
     public DateOnly PlanDate { get; set; }
@@ -20,11 +23,13 @@ public partial class ProductionPlanQuangNd
     public string? PlanStatus { get; set; }
 
     public int? PriorityLevel { get; set; }
-
+    [Range(0, int.MaxValue, ErrorMessage = "TotalExpectedQuantity must be a non-negative integer!")]
     public int? TotalExpectedQuantity { get; set; }
 
+    [Range(0, int.MaxValue, ErrorMessage = "TotalExpectedQuantity must be a non-negative integer!")]
     public int? ActualProducedQuantity { get; set; }
 
+    [StringLength(10, ErrorMessage = "BatchNote must have at least 10 characters!")]
     public string? BatchNotes { get; set; }
 
     public int? LastModifiedBy { get; set; }
